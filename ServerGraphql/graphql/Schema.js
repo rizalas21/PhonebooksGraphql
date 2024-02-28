e40daf2cb20629d480e60bd27a9a583359265a46
rizal anggara sudrajat <sudrajatrizal1@gmail.com>
2024-02-28 17:22:54 +0700

    create Back End

--- ServerGraphql/graphql/Schema.js
+++ ServerGraphql/graphql/Schema.js
@@ -31,24 +31,99 @@ type Query {
 type Mutation {
     createPhonebook(input: PhonebookInput): Phonebook
     updatePhonebook(_id: ID, input: PhonebookInput): Phonebook
-    deleteUser(_id: ID): Phonebook
+    deletePhonebook(_id: ID): Phonebook
 }
 `)
 
 const solution = {
     getPhonebooks: async ({ page = 1, limit = 30, keyword = '', sort = 'asc' }) => {
-        const offset = (page - 1) * limit
-        const data = await Contact.find().limit(limit).skip(offset)
-        console.log(data)
-        return {
-            phonebooks: data,
-            page,
-            limit,
+        try {
+            const offset = (page - 1) * limit
+            const data = await Contact.find().limit(limit).skip(offset)
+            return {
+                phonebooks: data,
+                page,
+                limit,
+            }
+        }
+        catch (err) {
+            console.log(err)
         }
     },
-    createUser: ({ input }) => Contact.create(input),
-    updateUser: ({ id, input }) => Contact.findOneAndUpdate(id, input, { new: true }),
-    deletePhonebook: ({ id }) => Contact.findByIdAndDelete(id)
+    createPhonebook: async ({ input }) => {
+        try {
+            const data = await Contact.create(input)
+            return data
+        } catch (err) {
+            console.log(err)
+        }
+    },
+    updatePhonebook: async ({ _id, input }) => {
+        try {
+            const data = await Contact.findOneAndUpdate(_id, input, { new: true })
+            return data
+        } catch (err) {
+            console.log(err)
+        }
+    },
+    deletePhonebook: async ({ _id }) => await Contact.findByIdAndDelete(_id)
 }
 
 module.exports = { schema, solution }
+
+/*
+query{
+getPhonebooks{
+        phonebooks {
+          _id,
+      name,
+      phone,
+      avatar
+        }
+    }
+}
+
+query{
+    getPhonebooks(page: 1, limit: 1){
+    page
+        phonebooks {
+            name
+    }
+  }
+}
+
+params: {
+    page,
+    any params
+}
+
+mutation {
+  createPhonebook(input: {name: "Rubi Henjaya", phone: "088123444"}) {
+    _id
+    name
+    phone
+    avatar
+  }
+}
+
+mutation {
+  deletePhonebook(_id: "65defaffd5659645d46732b0") {
+    _id
+    name
+    phone
+    avatar
+  }
+}
+
+mutation {
+  updatePhonebook(_id: "65db4bdc79812ce388dca088", input: {name: "m ramdani", phone: "08821896177810"}) {
+    _id
+    name
+    phone
+    avatar
+  }
+}
+
+
+*/
+
